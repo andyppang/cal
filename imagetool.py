5f2651cf68087df62b54aedc9d34ff1829780b22
andyppang <andyplpang@gmail.com>
2021-07-31 18:10:54 +0800

    图片压缩小程序

---
+++ imagetool.py
@@ -0,0 +1,21 @@
+from PIL import Image as img
+from tkinter import *
+from tkinter.filedialog import *
+
+
+info = {'path': []}
+def open_path():
+    filenames = askopenfilenames()
+    lbox = app.children['listbox']
+    info['path'] = filenames
+    if info['path']:
+        for name in filenames:
+            lbox.insert(END, name.split('/')[-1])
+
+app = Tk()
+app.geometry('300x400')
+Label(app, text='压缩图片小程序', font=('Arial', 20, 'bold')).pack()
+Listbox(app, name='listbox', bg='#f2f2f2').pack(fill=BOTH, expand=True)
+Button(app, text='打开路径', command=open_path).pack()
+Button(app, text='开始压缩').pack()
+app.mainloop()
