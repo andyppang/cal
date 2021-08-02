34421b13684c31d7cbc1d713af07874d5fa6ccda
andyppang <andyplpang@gmail.com>
2021-08-02 18:20:33 +0800

    增加水印功能

--- imagetool2.py
+++ imagetool2.py
@@ -10,7 +10,7 @@ from tkinter.filedialog import *
 
 # 创建空列表存放图片路径，全局变量供各个功能函数调用
 path = []
-
+logo_path = []
 
 # 打开路径
 def open_path():
@@ -45,22 +45,43 @@ def resize():
         image_small = image.resize((int(x), int(y)))    # 注意resize函数的大小为元组形式
         image_small.save(output + '变小' + name)  # 保存图片并改名
 
+def select_logo():
+    global logo_path
+    logo_path = askopenfilename()   # 水印图片地址
+
+
+def copy():
+    output = 'C:/Users/pl/Desktop/'
+    x = app.children['x'].get()  # 获取app窗口的名称为'x'的部件，即输入框的值
+    y = app.children['y'].get()  # 获取app窗口的名称为'y‘的部件，即输入框的值
+    for filepath in path:
+        name = filepath.split('/')[-1]  # 获取文件名
+        image = img.open(filepath)  # 读取图片
+        logo = img.open(logo_path)  # 读取logo图片
+        logo.thumbnail((100, 100))  # 将logo改成合适的大小
+        image_copy = image.copy()   # 复制图象
+        position = (int(x), int(y))           # 粘贴的位置
+        image_copy.paste(logo, position)
+        image_copy.save(output + '水印' + name)   # 保存图片并改名
+
 
 # 交互界面窗口
 def gui_app():
     app = Tk()
-    app.geometry('300x400')
+    app.geometry('320x400')
     Label(app, text='图片处理小程序', font=('Arial', 20, 'bold')).pack()
     Listbox(app, name='listbox', bg='#f2f2f2').pack(fill=BOTH, expand=True)
-    Button(app, text='打开路径', command=open_path).pack()
-    Button(app, text='压缩图片', command=compress).pack()
-    Button(app, text='调整大小', command=resize).pack(side=LEFT)
+    Button(app, text='打开路径', command=open_path).pack(side=TOP)
     e1 = StringVar()
     e1.set('宽')
     e2 = StringVar()
     e2.set('高')
-    Entry(app, name='x', textvariable=e1, width=12).pack(anchor=CENTER, fill=BOTH)
-    Entry(app, name='y', textvariable=e2, width=12).pack(anchor=CENTER, fill=BOTH)
+    Entry(app, name='x', textvariable=e1, width=12).pack(side=BOTTOM, fill=X)
+    Entry(app, name='y', textvariable=e2, width=12).pack(side=BOTTOM, fill=X)
+    Button(app, text='压缩图片', command=compress).pack(side=LEFT, fill=X, padx=1)
+    Button(app, text='调整大小', command=resize).pack(side=LEFT, fill=X, padx=1)
+    Button(app, text='添加水印', command=copy).pack(side=LEFT, fill=X, padx=1)
+    Button(app, text='水印图片', command=select_logo).pack(side=LEFT, fill=X, padx=1)
     return app
 
 
